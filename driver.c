af32b1645446029f0e1bcc2b531c6317c013470b
Soojin Choi <schoi4@stuy.edu>
2018-10-21 21:28:50 -0400

    broken sieve

--- driver.c
+++ driver.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "sieve.h"
+#include "sieve.c"
 
 int main(){
 
-  printf("%d\n", sieve(25));
+  printf("%d\n", sieve(1000000));
 
 }
--- sieve.c
+++ sieve.c
@@ -6,8 +6,39 @@
 
 int sieve (int n){
 
-  int size = (int)(n * log(n) * 1.15);
-  int table[size];
+  char size = (int)(n * log(n) * 1.15);
+  printf("%d -- %d", (int)log(n), size);
+  char table[size];
+  int factor = 2;
+  int curr_number = 1;
+  while (factor < size){
+
+    table[ factor ] = 0;
+    if (curr_number == n){
+
+      return factor;
+
+    }
+    else {
+
+      int multiple = factor + factor;
+      while (multiple < size || table[ multiple ] == 1){
+
+	table[ multiple ] = 1;
+	multiple = multiple + factor;
+	
+      }
+      
+    }
+    factor ++;
+    curr_number++;
+    while (table[ factor ] == 1){
+
+      factor++;
+
+    }
+    printf("factor -> %d \n current -> %d ", factor, curr_number);
+  }
   
   return size;
 
